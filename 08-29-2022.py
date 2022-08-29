10d20a7286b27a04fca83431d9f2b498690c2dc5
danielatuesta55 <danielatuesta55@gmail.com>
2022-08-29 16:58:20 -0700

    Added code first class

--- 08-29-2022.py
+++ 08-29-2022.py
@@ -1,1 +1,29 @@
-## This file has been created on the first day of class MSBA 502-503
+# This file has been created on the first day of class MSBA 502-503
+
+# Print your first line of code
+print("Hello, world!")
+
+# Print the following in the console
+"""
+*
+**
+*** 
+****
+
+"""
+#Print the shape from above
+print("*")
+print("**")
+print("***")
+print("****")
+
+#Now what happends if we change the form will it still print the same form?
+print("*")
+print("****")
+print("***")
+print("**")
+
+#As you can see the order does not stay the same as the begining and this is because 
+#The Python interpreter will read lines of code sequentially, starting with the first line of code
+#Lines at the top of your code execute before lines at the bottom of your code
+
