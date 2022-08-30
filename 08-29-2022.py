b65d15f0e49358c86464b2df0f3eff86f25d3d45
danielatuesta55 <danielatuesta55@gmail.com>
2022-08-29 17:08:31 -0700

    Added code first class

--- 08-29-2022.py
+++ 08-29-2022.py
@@ -27,3 +27,19 @@ print("**")
 #The Python interpreter will read lines of code sequentially, starting with the first line of code
 #Lines at the top of your code execute before lines at the bottom of your code
 
+#Variables
+
+# A variable is a container that we can use to store values
+# Each variable has unique name so that we can find, read, or update its value
+#Here is an example of a variable 
+
+text = "Hello, world!"
+print(text) #Here we use the print statement to display in the console our variable text
+
+#Assignment refers to giving a variable a value, which we indicate with an equal sign:
+text = "Hello, world!"
+motto = "Python is cool"
+
+print(text, motto)#The order in which you tell it to print will be how its printed. So in this case hello world is the first thing printed. 
+
+
