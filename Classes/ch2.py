07b7f5fe565197e8141ded5341dcd027d0dc9c39
danielatuesta55 <danielatuesta55@gmail.com>
2022-08-30 19:52:42 -0700

    Added ch 2

---
+++ Classes/ch2.py
@@ -0,0 +1,9 @@
+#Chapter one review 
+
+#another way of doing it is by writing a function like this
+def minutesinseconds(seconds_in_minute, minutes,existing_seconds):
+    total_seconds=(seconds_in_minute*minutes)+existing_seconds
+    return total_seconds
+print(minutesinseconds(60,42,22))
+
+rjust()
