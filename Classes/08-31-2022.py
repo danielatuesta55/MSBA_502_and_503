86bd50a39e7db463b63c4e0ee71c34eb81517319
danielatuesta55 <danielatuesta55@gmail.com>
2022-08-31 15:55:04 -0700

    Added new python file for 08-31-22 class

---
+++ Classes/08-31-2022.py
@@ -0,0 +1,15 @@
+#Today's Class
+""" Variables in python 
+
+A variable is a container tat we can use to store values
+
+Each variable has a unique name so that we can find, read, or update its value
+
+Example:
+"""
+
+text = "Hello, world!"
+motto = "Python is cool."
+print(text,motto)
+
+#Important to note that the teacher likes the syntax to be word_word_word make sure all your code is written in this format
