9e0c0bab4cd0b52e66a48137aaaee7ab7088d136
danielatuesta55 <danielatuesta55@gmail.com>
2022-10-06 22:57:28 -0700

    Added study session to repo

---
+++ Classes/.ipynb_checkpoints/08-29-2022-checkpoint.py
@@ -0,0 +1,45 @@
+# This file has been created on the first day of class MSBA 502-503
+
+# Print your first line of code
+print("Hello, world!")
+
+# Print the following in the console
+"""
+*
+**
+*** 
+****
+
+"""
+#Print the shape from above
+print("*")
+print("**")
+print("***")
+print("****")
+
+#Now what happends if we change the form will it still print the same form?
+print("*")
+print("****")
+print("***")
+print("**")
+
+#As you can see the order does not stay the same as the begining and this is because 
+#The Python interpreter will read lines of code sequentially, starting with the first line of code
+#Lines at the top of your code execute before lines at the bottom of your code
+
+#Variables
+
+# A variable is a container that we can use to store values
+# Each variable has unique name so that we can find, read, or update its value
+#Here is an example of a variable 
+
+text = "Hello, world!"
+print(text) #Here we use the print statement to display in the console our variable text
+
+#Assignment refers to giving a variable a value, which we indicate with an equal sign:
+text = "Hello, world!"
+motto = "Python is cool"
+
+print(text, motto)#The order in which you tell it to print will be how its printed. So in this case hello world is the first thing printed. 
+
+
